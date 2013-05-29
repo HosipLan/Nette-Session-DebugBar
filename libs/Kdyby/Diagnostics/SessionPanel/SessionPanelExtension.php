23066cb4e2a9d9ef83307a3a94f10917edc2c9e2
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2013-05-29 13:23:32 +0200

    Add other aliases

--- libs/Kdyby/Diagnostics/SessionPanel/SessionPanelExtension.php
+++ libs/Kdyby/Diagnostics/SessionPanel/SessionPanelExtension.php
@@ -6,7 +6,9 @@ use Nette;
 
 if (!class_exists('Nette\DI\CompilerExtension')) {
 	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
+	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
 	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
+	class_alias('Nette\Config\Helpers', 'Nette\DI\Helpers');
 }
 
 /**
